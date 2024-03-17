4b1cff34cb434c94fba1da3b3957eec5de6ced6b
EstrHuP <estrhup@gmail.com>
2024-03-17 17:11:07 +0100

    Initial commit

---
+++ QRCodeScanner-iOS/QRCodeScanner_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScanner_iOSApp.swift
+//  QRCodeScanner-iOS
+//
+//  Created by EstrHuP on 17/3/24.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScanner_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
